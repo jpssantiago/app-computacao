c6f451b62f4f243ca3e9d9697e69be14b3f00235
jpssantiago <jps_santiago@outlook.com>
2021-04-26 17:03:48 -0300

    Adicionado página do produto.

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
+import getProducts from '../../assets/data/products';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.page.html',
   styleUrls: ['./product.page.scss'],
 })
 export class ProductPage implements OnInit {
+
+  public product = getProducts()[0];
+  public amount : number = 1;
 
   constructor(private navController : NavController) { }
 
@@ -15,5 +20,14 @@ export class ProductPage implements OnInit {
 
   public back = () => {
     this.navController.pop();
+  }
+
+  public decrement = () => this.amount--;
+
+  public increment = () => this.amount++;
+
+  public calculateTotalPrice = () : number => {
+    let price = this.product.price;
+    return price * this.amount;
   }
 }
