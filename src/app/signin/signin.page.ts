d62552f52ea75a1c7216bddb64779f8e118ef6f4
Apolo <apolo.argolo@gmail.com>
2021-06-02 23:01:01 -0300

    Loading na pagina de login

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { OverlayBaseController } from '@ionic/angular/util/overlay';
 import { UserService } from '../services/user.service';
 
@@ -16,19 +16,21 @@ export class SigninPage implements OnInit {
   public email: string;
   public password: string;
   public firebaseSubscription;
+  private loading: any;
 
   constructor(
     private router: Router,
     private firebaseAuth: AngularFireAuth,
     private firebaseStorage: AngularFirestore,
     private userService: UserService,
-    private toastController: ToastController
+    private toastController: ToastController,
+    private loadingController: LoadingController,
   ) { }
 
   ngOnInit() {
   }
 
-  public async signin() {
+  public async signin() {   
     try {
       const response = await this.firebaseAuth.signInWithEmailAndPassword(this.email, this.password);
       if (response) {
@@ -44,7 +46,8 @@ export class SigninPage implements OnInit {
           this.email = '';
           this.password = '';
         });
-      }
+        await this.presentLoading();
+      } 
     } catch(err) {
       const { code } = err;
       let message: string;
@@ -59,11 +62,21 @@ export class SigninPage implements OnInit {
         message = 'Um erro inesperado ocorreu, tente novamente.';
         console.log(err);
       }
-
       this.toastController.create({
         message,
         duration: 2000,
       }).then(toast => toast.present());
-    }
+      await this.presentLoading();
+      this.loading.dismiss();
+    } 
+    finally {
+      this.loading.dismiss();
+  }
+  }
+ async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Por favor, aguarde...'
+    });
+    return this.loading.present();
   }
 }
